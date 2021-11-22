11ba1a8870abf67b9d99e2a7210624f7f016fe4c
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-11-22 21:32:27 +0000

    build-> header and footer

--- views/main/main.php
+++ views/main/main.php
@@ -1,18 +1,13 @@
 <!-- This is the main generic view of your application, it's not required to use it -->
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include_once "./views/include/header"
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-</head>
+<h1>Welcome to MVC Pattern Basics!</h1>
+<div class="list-group">
+    <a class="list-group-item list-group-item-action" href="?controller=users&action=getAllUsers">Users Controller</a>
 
-<body>
-    <h1>Welcome to MVC Pattern Basics!</h1>
-    <div class="list-group">
-        <a class="list-group-item list-group-item-action" href="?controller=users&action=getAllUsers">Users Controller</a>
-</body>
-
-</html>
+</div>
+<?php
+include_once "../include/footer.html"
+?>
