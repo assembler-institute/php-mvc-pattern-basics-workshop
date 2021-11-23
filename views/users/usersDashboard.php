2cceb9bf77658f82509bd88e846db7c17eff9a7d
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-11-23 19:44:11 +0000

    final

--- views/users/usersDashboard.php
+++ views/users/usersDashboard.php
@@ -4,7 +4,7 @@
         <br>
         <?php
         foreach ($users as $user) {
-            echo ' <div class="col-sm-2"> <div class="card"> <div class="card-body">';
+            echo ' <div class="col-sm-2 m-2"> <div class="card"> <div class="card-body">';
             echo "<a href=?controller=anotations&action=getAnotationsById&id=" . $user['id'] . "&name=" . $user['name'] . ">" . "<h2 class='text-center'>" . $user["name"] . "</h2>" . "</a>";
             echo "<a href=?controller=users&action=getUser&id=" . $user['id'] . ">" . "<button type='button' class='btn btn-success btn-sm m-1'>Edit</button>" . "</a>";
             echo "<a href=?controller=users&action=deleteUser&id=" . $user['id'] . ">" . "<button type='button' class='btn btn-danger btn-sm m-1'>Delete</button>" . "</a>" . "<br>";
