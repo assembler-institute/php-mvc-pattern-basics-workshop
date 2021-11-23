d8797160b54ae53d2a251b39dac27be48f50bea1
tamtran <tamtran2885@gmail.com>
2021-11-23 19:54:38 +0100

    fix sign up form

--- views/signup/signupDashBoard.php
+++ views/signup/signupDashBoard.php
@@ -23,19 +23,19 @@
         >
             <div class="mb-3">
                 <label for="username" class="form-label">Username:</label>
-                <input type="text" class="form-control" id="username"  name="username"  autocomplete="current-name">
+                <input type="text" class="form-control" id="username"  name="username"  autocomplete="current-name" required>
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email:</label>
-                <input type="email" class="form-control" id="email"  name="email"  autocomplete="current-name">
+                <input type="email" class="form-control" id="email"  name="email"  autocomplete="current-name" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password:</label>
-                <input type="password" class="form-control" id="password"  name="password" autocomplete="current-password">
+                <input type="password" class="form-control" id="password"  name="password" autocomplete="current-password" required>
             </div>
             <div class="mb-3">
                 <label for="confirmPassword" class="form-label">Password:</label>
-                <input type="password" class="form-control" id="confirmPassword"  name="confirmPassword" autocomplete="current-password">
+                <input type="password" class="form-control" id="confirmPassword"  name="confirmPassword" autocomplete="current-password" required>
             </div>
             <button type="submit" class="btn btn-primary" value="Submit">Submit</button>
         </form>
