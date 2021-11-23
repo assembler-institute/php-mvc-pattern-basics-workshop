4dd56f57379bb8fe1d6db824774d4ffee0057e0a
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-11-23 12:55:40 +0000

    feat-> db changes

--- resources/db.sql
+++ resources/db.sql
@@ -12,33 +12,43 @@ password VARCHAR(50) NOT NULL,
 avatar VARCHAR(200)
 );
 
-CREATE TABLE categories(
+CREATE TABLE subjects(
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-category_name VARCHAR(50) NOT NULL
+subjects_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE posts(
+CREATE TABLE anotations(
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-post_user_id INT NOT NULL,
-category_id INT NOT NULL,
+anotations_user_id INT NOT NULL,
+subjects_id INT NOT NULL,
 title VARCHAR(50),
-description TEXT(1000),
-date DATE, 
-time TIME,  
+grades DECIMAL(6,2),
+date DATE,  
 FOREIGN KEY (post_user_id) REFERENCES users(id), 
-FOREIGN KEY (category_id) REFERENCES categories(id) 
+FOREIGN KEY (subjects_id) REFERENCES subjects(id) 
 );
 
 -- Insert of data
-INSERT INTO users (name, last_name, email, password, avatar)
+INSERT INTO users (name, last_name, email, age, avatar)
 VALUES 
-("Rack", "Lei", "jackon@network.com", "123456789", "https://pbs.twimg.com/profile_images/587511475440332800/_Y3Wl3PL.jpg"),
-("John", "Doe", "jhondoe@foo.com", "123456789", "https://pbs.twimg.com/profile_images/1094979667143069696/QrD0ovrh.jpg"),
-("Leila", "Mills", "mills@leila.com", "123456789", "https://m.media-amazon.com/images/M/MV5BMzI5NDIzNTQ1Nl5BMl5BanBnXkFtZTgwMjQ0Mzc1MTE@._V1_UY256_CR4,0,172,256_AL_.jpg");
+("Rack", "Lei", "jackon@network.com", 21, "https://pbs.twimg.com/profile_images/587511475440332800/_Y3Wl3PL.jpg"),
+("John", "Doe", "jhondoe@foo.com", 22, "https://pbs.twimg.com/profile_images/1094979667143069696/QrD0ovrh.jpg"),
+("Leila", "Mills", "mills@leila.com", 23, "https://m.media-amazon.com/images/M/MV5BMzI5NDIzNTQ1Nl5BMl5BanBnXkFtZTgwMjQ0Mzc1MTE@._V1_UY256_CR4,0,172,256_AL_.jpg");
 
-INSERT INTO categories (category_name)
-VALUES ("Example Category");
+INSERT INTO subjects (subjects_name)
+VALUES ("Maths");
 
 
-INSERT INTO posts (post_user_id,category_id,title,description,date,time)
-VALUES (1,1, "Example Title","Lorem ipsum dolor sit amet, consectetur adipiscing elit...", "2021-11-11", "13:23:44");
+INSERT INTO anotations (anotations_user_id,subjects_id,title,grades,date)
+VALUES (1,1, "Surprise Exam",4.9, "2021-11-11");
+
+CREATE TABLE anotations(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+post_user_id INT NOT NULL,
+subjects_id INT NOT NULL,
+title VARCHAR(50),
+grades DECIMAL(6,2),
+date DATE,  
+FOREIGN KEY (post_user_id) REFERENCES users(id), 
+FOREIGN KEY (subjects_id) REFERENCES subjects(id) 
+);
