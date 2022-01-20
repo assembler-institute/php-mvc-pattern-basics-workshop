8a3f6caef4b033c7c2de55c8554d1ad0500944da
Garikoitz Herrero Vargas <85669728+GariHV@users.noreply.github.com>
2022-01-20 12:53:02 +0100

    shiny update

---
+++ assets/js/utilsShiny.js
@@ -0,0 +1,47 @@
+
+
+var pokemonIdData = document.getElementById("pokemonIdData");
+var pokemonSelectedShiny = document.getElementsByClassName("pickAPokemonShiny");
+var cInfo= document.getElementById("cInfo");
+var mInfo= document.getElementById("mInfo");
+var btnPokedexCloseShiny = document.getElementById("cerrarPokedex");
+var btnPokedexNormal = document.getElementById("modoNormal");
+var btnPokedexSearchShiny = document.getElementById("buscadorPokemon");
+
+btnPokedexCloseShiny.addEventListener("click", function(){
+    window.location.replace("./index.php")
+})
+
+btnPokedexNormal.addEventListener("click", function(){
+    console.log('a');
+    window.location.replace("./index.php?controller=pokemon&action=getAllPokemons")
+})
+
+
+for (const iterator of pokemonSelectedShiny) {
+    iterator.addEventListener("click", function(e){
+        if(e.path[2].id !== ""){
+            window.location.replace("./index.php?controller=pokemonShiny&action=getPokemon&id="+ e.path[2].id)
+        } else{
+            window.location.replace("./index.php?controller=sefasdf")
+        }
+    })
+}
+
+function hover(element) {
+    console.log(pokemonIdData);
+    element.setAttribute('src', "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/" + pokemonIdData.innerHTML + ".png");
+}
+
+function unhover(element) {
+    element.setAttribute('src', "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + pokemonIdData.innerHTML + ".png");
+}
+
+cInfo.addEventListener("click",function(e){
+    window.location.replace("./index.php?controller=combat&action=combat&id="+e.target.dataset.id);
+})
+
+mInfo.addEventListener("click",function(e){
+    window.location.replace("https://www.pokemon.com/es/pokedex/"+e.target.dataset.name);
+})
+
--- views/pokemon/PokedexShiny.php
+++ views/pokemon/PokedexShiny.php
@@ -7,12 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
     <link href="assets/css/style.css" rel="stylesheet">
-    <script src="assets/js/utils.js" defer></script>
+    <script src="assets/js/utilsShiny.js" defer></script>
 </head>
 <body>
 <nav id="botonesPokedex">
     <div id="cerrarPokedex" class="btnPokedex"></div>
-    <div id="modoShiny" class="btnPokedex"></div>
+    <div id="modoNormal" class="btnPokedex"></div>
     <div id="buscadorPokemon" class="btnPokedex"></div>
   </nav>
   <div id="tablaTodos" class="overflow-auto">
@@ -26,12 +26,12 @@
         </tr>
       </thead>
       <tbody>
-      <?php 
+      <?php
       foreach($pokemons as $pokemon){
       ?>
-        <tr id="<?=$pokemon["id"]?>" class="pickAPokemon">
+        <tr id="<?=$pokemon["id"]?>" class="pickAPokemonShiny">
           <th scope="row"><?= $pokemon["id"]?></th>
-          <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/<?= $pokemon["id"]?>.png" alt="">  </td>
+          <td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/<?= $pokemon["id"]?>.png" alt="">  </td>
           <td><?= $pokemon["name"];?></td>
           <td><?php foreach($types as $type){
                 if($pokemon["tipo1"]==$pokemon["tipo2"]){
@@ -47,42 +47,14 @@
                       echo "<img src=".$type["img"].">";
                   }
                 }
-                }
-        ?></td>
+            }
+          ?></td>
         </tr>
         <?php
     } ?>
 
-    </tbody>
+      </tbody>
     </table>
-</div>
-<?php  ?>
-<div id="pokemon" class="pokemonData">
-        <h3><?= $pokemonSelectedTodo1[0]["name"];?></h3>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/<?= $pokemonSelectedTodo1[0][0]?>.png" alt="" onmouseover="hover(this)" onmouseout="unhover(this)">
-        
-        <p><?php
-        foreach($pokemonSelectedTodo2 as $type){
-                if($pokemonSelectedTodo1[0]["tipo1"]==$pokemonSelectedTodo1[0]["tipo2"]){
-                if($pokemonSelectedTodo1[0]["tipo1"]==$type["id"]){
-                    echo "<img src=".$type["img"].">";
-                    }
-                }else{
-                if($pokemonSelectedTodo1[0]["tipo1"]==$type["id"]){
-                    echo "<img src=".$type["img"].">";
-                }
-                if($pokemonSelectedTodo1[0]["tipo2"]==$type["id"]){
-                    echo "<img src=".$type["img"].">";
-                }
-                }
-            }
-        ?></p>
-
-<div id="pokemonIdData"><?= $pokemonSelectedTodo1[0][0]?></div>
-        <button class="" id="cInfo" data-id="<?= $pokemonSelectedTodo1[0][0]?>">Combat Information</button>
-        <button class="" id="mInfo" data-name="<?= $pokemonSelectedTodo1[0]["name"];?>">More Info</button>
   </div>
 </body>
-
-
 </html>
