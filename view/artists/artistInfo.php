0d5971750f90dd302f50714124351f22ccd599c2
LuisMM24 <luismateo2416@gmail.com>
2022-01-20 18:03:45 +0100

    artist info display

--- view/artists/artistInfo.php
+++ view/artists/artistInfo.php
@@ -7,13 +7,21 @@
 </head>
 
 <body>
-    <?php require_once(BASE_PATH . "/assets/" . "templates/header.html"); ?>
+    <?php
+    require_once(BASE_PATH . "/assets/" . "templates/header.html"); ?>
 
     <h1 class="text-center"><?= $info["first_name"] . " " . $info["last_name"] ?></h1>
-    <main class="ms-3 row row-cols-1 row-cols-md-2  row-cols-xl-4 g-3">
-        <img src="assets/img/<?= $info["artist_photo"] ?>" class="rounded mx-auto d-block" alt="...">
-        <!-- TODO INSERT CARROUSELL -->
-        <!-- TODO INSERT CONTAINER  -->
+    <main class="d-flex justify-content-center gap-5">
+        <img class="productImg rounded" src="<?= ASSETS . "img/" . $info["artist_photo"] ?>">
+        <article class="d-flex flex-column justify-content-center information">
+            <h4>First name: <?= $info["first_name"] ?></h4>
+            <h4>Last name: <?= $info["last_name"] ?></h4>
+            <hr>
+            <h5>History: <?= $info["information"] ?></h5>
+        </article>
+    </main>
+    <!-- TODO INSERT CARROUSELL -->
+    <!-- TODO INSERT CONTAINER  -->
 
     </main>
 </body>
